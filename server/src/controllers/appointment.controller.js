e83d75014b7d36333c5831f42a629552ad50f9a9
vinayak5490 <149044261+vinayak5490@users.noreply.github.com>
2026-07-18 00:05:35 +0530

    get appointment controller created

--- server/src/controllers/appointment.controller.js
+++ server/src/controllers/appointment.controller.js
@@ -28,3 +28,30 @@ export const createAppointment = async (req, res)=>{
         res.status(500).json({success: false, message: error.message});
     }
 }
+
+// @desc    Get All Appointments (With Search & Status Filters)
+// @route   GET /api/appointments
+// @access  Protected (Admin Only)
+
+export const getAllAppointments = async(req, res)=>{
+    try {
+        const { status, search } = req.query;
+        let queryEngine = {};
+        if(status && status !== "All"){
+            queryEngine.status = status;
+        }
+
+        if(search){
+            queryEngine.$or = [
+                {patientName: {$regex: search, $options: "i"}},
+                {phone: {$regex: search, $options: "i"}},
+                {bookingId: {$regex: search, $options; "i"}},
+            ];
+        }
+
+        const records = await Appointment.find(queryEngine).sort({ createdAt: -1});
+        res.status(200).json({success: true, count: records.length, date: records});
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message});
+    }
+};
