5ffc5812becb041f590f491bfef00ffe388ad806
vinayak5490 <149044261+vinayak5490@users.noreply.github.com>
2026-07-23 15:40:04 +0530

    make the configuration in the vite.config.js to connect the frontent to backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-  
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+});
