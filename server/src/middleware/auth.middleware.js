c73539fedbc11c8211e73d2361cf8c501b56d135
vinayak5490 <149044261+vinayak5490@users.noreply.github.com>
2026-07-29 15:11:01 +0530

    corrected the auth middleware in the server

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -2,28 +2,34 @@ import jwt from "jsonwebtoken";
 import Admin from "../models/Admin.js";
 
 export const protectAdmin = async(req, res, next) =>{
-    let token;
+    try {
+        //Reach token from HttpOnly cookie
+        const token = req.cookies.adminToken;
 
-    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
-        try {
-            token = req.headers.authorization.split(" ")[1];
-
-            //Decode JWT token
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-            //Find admin and attach profile context (excluding sensitive password hashes)
-            req.admin = await Admin.findById(decode.id).select("-password");
-
-            if(!req.admin){
-                return res.status(401).json({success: false, message: "Administratives context not found."});
-            }
-            // return next();
-        } catch (error) {
-            console.error("JWT Error:", error.message);
-            return res.status(401).json({success: false, message: "Not authorized: Token signature has expired or is invalid."});
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Not authorized: No authentication token found.",
+            });
         }
-    }
-    if(!token){
-        return res.status(401).json({success: false, message: "Not authorized: No authentication credentials provided."});
+
+        //verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        //Attach admin context
+        req.admin = await Admin.findById(decoded.id).select("-password");
+
+        if(!req.admin){
+            return res.status(401).json({
+                success: false,
+                message: "Admin profile not found.",
+            });
+        }
+    } catch (error) {
+        console.error("Auth Error:", error.message);
+        return res.status(401).json({
+            success: false,
+            message: "Not authorized: Token has expired or is invalid",
+        });
     }
 };
