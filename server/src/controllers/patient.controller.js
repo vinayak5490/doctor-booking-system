29d10326ed2eaf0f480164113e147e15ef429f78
vinayak5490 <149044261+vinayak5490@users.noreply.github.com>
2026-08-02 21:50:02 +0530

    created patient controller

---
+++ server/src/controllers/patient.controller.js
@@ -0,0 +1,48 @@
+import Patient from "../models/Patient";
+
+//Create New Patient Record
+export const createPatient = async(req, res)=>{
+    try {
+        const newPatient = new Patient(req.body);
+        const saved = await newPatient.save();
+        res.status(201).json({ success: true, data: saved });
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message});
+    }
+};
+
+//Get Patients with Pagination & Search
+export const getPatients = async (req, res)=>{
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const search = req.query.search || '';
+
+        const query = search
+        ?{
+            $or:[
+                { patientName: { $regex: search, $options: 'i'}},
+                { phone: { $regex: search, $options: 'i'}},
+            ],
+        }
+        :{};
+
+        const total = await Patient.countDocuments(query);
+        const patients = await Patient.find(query)
+        .skip((page-1) * limit)
+        .limit(limit)
+        .sort({createdAt: -1});
+
+        res.status(200).json({
+            success: true,
+            total,
+            totalPages: Math.ceil(total/limit),
+            page,
+            data: patients,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false, message: error.message
+        })
+    }
+}
